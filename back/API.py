a90d3302658313aba3a286ddfd126abe0ccf38dc
JaedenCastro <b2025jmgcastro@pshs.edu.ph>
2024-05-24 22:33:49 +0800

    made commits to remove trees

--- back/API.py
+++ back/API.py
@@ -1,18 +1,11 @@
-import requests
-import json
-#Get data from forex API
+from fastapi import FastAPI
 
-request = requests.get("https://v6.exchangerate-api.com/v6/31e6837d9bf5717e3d305eaf/latest/USD")
-#Check if API works
-print(request.status_code)
-forex_data = request.json()
-#start converting
-conversion_rates = forex_data["conversion_rates"]
-USD = conversion_rates["USD"]
-PHP = conversion_rates["PHP"]
-#inputs
-currencyIn = conversion_rates[input("Enter Currency 1 (Input Currency) Here:   ")] 
-currencyOut = conversion_rates[input("Enter Currency 1 (Output Currency) Here:   ")]
-valueIn = float(input("Enter amount of currency 1 you want to convert (float):   "))
-
-print(valueIn/currencyIn*currencyOut)
+app = FastAPI()
+"""
+try this:
+python -m uvicorn API:app --reload
+cd C:\Users\jaede\OneDrive\Documents\!!! school things\11th\res\XHub-arXiv-Warriors-Hub-\back
+"""
+@app.get("/example")
+async def read_root():
+    return {"Message": "Congrats! This is your first API!", "data": 0}
