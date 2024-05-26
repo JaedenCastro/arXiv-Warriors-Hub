b3e53ada991e02f6e9fb6d03b8166fd655762edd
JaedenCastro <b2025jmgcastro@pshs.edu.ph>
2024-05-26 18:00:21 +0800

    updating random stuff

--- back/API.py
+++ back/API.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
-"""
-try this:
-python -m uvicorn API:app --reload
-cd C:\Users\jaede\OneDrive\Documents\!!! school things\11th\res\XHub-arXiv-Warriors-Hub-\back
-"""
-@app.get("/example")
+
+# try this:
+# python -m uvicorn API:app --reload
+# cd C:\Users\jaede\OneDrive\Documents\!!! school things\11th\res\XHub-arXiv-Warriors-Hub-\back
+
+@app.get("/AI")
 async def read_root():
-    return {"Message": "Congrats! This is your first API!", "data": 0}
+    rn = random.randint(0,100) 
+    return {"Message": "Congrats! This is your first API!", "data": 10, "random_number" : rn}
