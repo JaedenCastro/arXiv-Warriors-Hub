89dea3f52d460f5bc6ef5432b5ec4777e177a5bb
SyedBarkiaNHashemiNik <b2025sbnhasheminik@pshs.edu.ph>
2024-09-06 12:30:18 +0800

    Comitted model to Hub

---
+++ back/model/pythonProject/Filipino-Text-Benchmarks-master/Filipino-Text-Benchmarks-master/utils/testing.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\TUF\PycharmProjects\pythonProject\Filipino-Text-Benchmarks-master\Filipino-Text-Benchmarks-master\data\newsproviders\testCopy.csv")
+print(df.columns.to_list())
